2934b7b2e2f9eef1499b8151ebaeb926d1bad2d3
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-10-28 15:48:39 -0300

    update

--- my-app/src/components/comunication/IndirectFather.js
+++ my-app/src/components/comunication/IndirectFather.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import IndirectSon from './IndirectSon';
 
 export default props => {
+    const [name, setName] = useState('?');
+    const [age, setAge] = useState(0);
+    const [nerd, setNerd] = useState(true);
+
     function getInfo(name, age, nerd) {
-        console.log(name, age, nerd);
+        setName(name);
+        setAge(age);
+        setNerd(nerd);
     };
-    
-    return(
+
+    return (
         <div>
-            pai
-            <IndirectSon onClick={getInfo}/>
+            <span>{name}</span>
+            <span>{age}</span>
+            <span>{nerd ? 'Verdadeiro' : 'Falso'}</span>
+
+            <IndirectSon onClick={getInfo} />
         </div>
     )
 }
--- my-app/src/components/comunication/IndirectSon.js
+++ my-app/src/components/comunication/IndirectSon.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 export default props => {
+    const cb = props.onClick;
+    const min = 20;
+    const max = 50;
+    const getAge = () => parseInt(Math.random() * (max - min) + min);
     return(
         <div>
             <div>filho</div>
-            <button onClick={() => {
-                props.onClick('Cleiton', 44, true)
-            }}>Fornecer informações</button>
+            <button onClick={_ => cb('Cleiton', getAge(), true)}
+            >Fornecer informações</button>
         </div>
     )
 }
