378bbbfe9cf0ecd65e0ea5f4961f589889369856
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-10-27 23:22:50 -0300

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,17 +4,23 @@ import Card from './components/layout/Card'
 import First from './components/basics/First';
 import WithParams from './components/basics/WithParams';
 import Number from './components/basics/Number';
+import Family from './components/basics/Family';
 
 export default props =>
     <div id="app">
         <First />
         <div className="cards">
 
-            <Card title="Valor aleatório" color="#fe4a49">
+            <Card title="#03 Componente com filhos" color="#fed766">
+                <Family />
+            </Card>
+
+            <Card title="#02 Valor aleatório" color="#fe4a49">
                 <Number minValue={0} maxValue={10} />
             </Card>
 
-            <Card title="Com parâmetros e fragmento" color="#2ab7ca">
+
+            <Card title="#01 Com parâmetros e fragmento" color="#2ab7ca">
                 <WithParams name="Amanda" note={9} />
             </Card>
         </div>
---
+++ my-app/src/components/basics/Family.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FamilyMember from './FamilyMember';
+
+export default props => {
+    return(
+        <div>
+            <FamilyMember name="Amanda" />
+            <FamilyMember name="João" />
+            <FamilyMember name="Pedro" />
+        </div>
+    )
+}
